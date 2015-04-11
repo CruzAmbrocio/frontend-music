dcfd6cf15402f5334c08845203455d33dc60f3e8
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-04-10 18:29:04 -0600

    se ha hecho la llamada al json

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// Javascript Code
+jQuery(document).ready(function($) {
+  $.ajax({
+  url : "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=16d8b076f9808ebbd4b7908e74fe1154&format=json",
+  dataType : "json",
+  success : function(parsed_json) {
+  var hola = parsed_json['tracks']['track']['0']['name'];
+  var haha = parsed_json['tracks']['track']
+  alert("Current temperature in "+hola);
+  $('.hola').append( "<p>"+hola+"  </p>");
+  console.log(haha);
+  }
+  });
+});
