344e78e30720af02f5d2660f77e91ce15f5829ee
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-04-13 10:41:36 -0600

    Botones funcionales
    se ha hecho que los botones se seleccionen
    se mejora un poco la presentacion

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ var varName = [];
 var varDuration = [];
 var varArtist = [];
 var varImage = [];
+var azZA="";
 
 jQuery(document).ready(function($) {
   $.ajax({
   url : "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=16d8b076f9808ebbd4b7908e74fe1154&format=json",
   dataType : "json",
   success : function(parsed_json) {
+
     for (var i = 0; i <= 50; i++) {
         playCounts.push(parsed_json["tracks"]["track"][i]["playcount"])
         varDuration.push(parsed_json["tracks"]["track"][i]["duration"])
@@ -31,18 +33,64 @@ jQuery(document).ready(function($) {
             "</div>"+
             "<div class='col-md-8'>"+
                 "<div class='infoSong'>"+
-                    "<p class='name'> "+varName[i]+"</classp>"+
-                    "<p class='artist'>"+varArtist[i]+"</p>"+
-                    "<p class='duration'>"+varDuration[i]+"  Seg."+"</p>"+
+                    "<p class='name'>"+"Name  : "+varName[i]+"</classp>"+
+                    "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                    "<p class='duration'>"+"Duration  : "+varDuration[i]+"  Seg."+"</p>"+
+                    "<p class='playCounts'>"+"Play counts  : "+playCounts[i]+"</p>"+
                 "</div>"+
             "</div>"+
         "</div>"
             );
-
     }
+
   }
   });
 });
+
+/*
+public class Prueba {
+  public static void main(String[] args) {
+
+    String a[] = {"Ramon", "Maria", "Juan"};
+
+    ArrayList lista = new ArrayList(); 
+
+    for(int i = 0; i < a.length; i++){
+
+      lista.add(a[i]);
+
+    }
+
+    Collections.sort(lista);
+
+    System.out.println(lista);
+
+
+
+  }
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  $(".bottons").hover(
       function() {
@@ -52,8 +100,62 @@ jQuery(document).ready(function($) {
         $(this).removeClass("hover_button");
     });
 
-    $('.filters').click(function() {
-        $(this).addClass(".selecfiltr");
+ $("#Top").click(
+      function() {
+        $("#Top").addClass("selecfiltr");
+        $("#time").removeClass("selecfiltr");
+        $("#alpha").removeClass("selecfiltr");
+        $(".oculto").removeClass("selecfiltr");
+        $("#counts").removeClass("selecfiltr");
     });
 
+
+ $("#counts").click(
+      function() {
+        $("#Top").removeClass("selecfiltr");
+        $("#counts").addClass("selecfiltr");
+        $("#time").removeClass("selecfiltr");
+        $("#alpha").removeClass("selecfiltr");
+        $(".oculto").removeClass("selecfiltr");
+        $("#top").removeClass("selecfiltr");
+    });
+
+
+
+ $("#time").click(
+      function() {
+        $("#Top").removeClass("selecfiltr");
+        $("#time").addClass("selecfiltr");
+        $("#counts").removeClass("selecfiltr");
+        $("#alpha").removeClass("selecfiltr");
+        $(".oculto").removeClass("selecfiltr");
+        $("#top").removeClass("selecfiltr");
+    });
+
+
+
+ $("#alpha").click(
+      function(event) {
+        $(".oculto").addClass("selecfiltr");
+        $("#alpha").addClass("selecfiltr");
+        $("#counts").removeClass("selecfiltr");
+        $("#time").removeClass("selecfiltr");
+        $("#Top").removeClass("selecfiltr");
+        $("#alpha").click(function(event){
+        event.preventDefault();
+        $(".oculto").show("fast");
+        $(".muestra").hide("fast");
+        });
+        $(".oculto").click(function(event){
+        event.preventDefault();
+        $(".muestra").show(100);
+        $(".oculto").hide(100);
+        $(".oculto").addClass("selecfiltr");
+        $("#alpha").addClass("selecfiltr");
+        $("#counts").removeClass("selecfiltr");
+        $("#time").removeClass("selecfiltr");
+        $("#Top").removeClass("selecfiltr");
+        });
+
+    });
 
