9ef4eb53e1d36b1013715417d8684c813b960b56
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-04-12 20:00:07 -0600

    se ha hecho cambios a la presentación se hace las primeras llamadas al Json y se añaden al html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,55 @@
+// Javascript Code.
+var playCounts = [];
+var varName = [];
+var varDuration = [];
+var varArtist = [];
+var varImage = [];
+
 jQuery(document).ready(function($) {
   $.ajax({
   url : "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=16d8b076f9808ebbd4b7908e74fe1154&format=json",
   dataType : "json",
   success : function(parsed_json) {
-  var hola = parsed_json['tracks']['track']['0']['name'];
-  var haha = parsed_json['tracks']['track']
-  console.log(hola);
-  $('.hola').append( "<p>"+hola+"  </p>");
-  console.log(haha);
+    for (var i = 0; i <= 50; i++) {
+        playCounts.push(parsed_json["tracks"]["track"][i]["playcount"])
+        varDuration.push(parsed_json["tracks"]["track"][i]["duration"])
+        varName.push(parsed_json["tracks"]["track"][i]["name"])
+        varArtist.push(parsed_json['tracks']['track'][i]['artist']['name'])
+        try {varImage.push(parsed_json["tracks"]["track"][i]["image"][2]["#text"])}
+        catch(err){varImage.push("images/undefined.jpg")}
+            console.log(playCounts);
+            console.log(varImage);
+
+          $('.songs').append(
+        "<div class='col-md-8 col-md-offset-1'>"+
+            "<hr/>"+
+            "<div class='col-md-2'>"+
+                "<p class='noSong'>"+(i+1)+"</p>"+
+            "</div>"+
+            "<div class='col-md-2'>"+
+                "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+            "</div>"+
+            "<div class='col-md-8'>"+
+                "<div class='infoSong'>"+
+                    "<p class='name'> "+varName[i]+"</p>"+
+                    "<p class='artist'>"+varArtist[i]+"</p>"+
+                    "<p class='duration'>"+varDuration[i]+"  Seg."+"</p>"+
+                "</div>"+
+            "</div>"+
+        "</div>"
+);
+
+    }
   }
   });
 });
+
+ $(".bottons").hover(
+      function() {
+        $(this).addClass("hover_button");
+    },
+      function() {
+        $(this).removeClass("hover_button");
+    });
+
+
