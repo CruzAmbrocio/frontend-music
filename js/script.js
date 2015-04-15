00a2c879107749726b3481705b7543f23f568f8f
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-04-15 10:50:19 -0600

    se ha podido ordenar conforme todos los filtros faltaria mejorar los botones y presentacion tambien mejora del codigo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 // Javascript Code.
 var playCounts = [];
 var playorder =[];
+
 var varName = [];
+var namOrder =[];
+
 var varDuration = [];
+var durationOrder=[];
+
 var varArtist = [];
+
 var varImage = [];
 
  $(".bottons").hover(
@@ -14,10 +20,27 @@ var varImage = [];
         $(this).removeClass("hover_button");
     });
 
-function orderplay(first, second){
-first = first/60
-second = second/60
-if (first<second) {return 1}else{return 0};
+function orderPlay(first, second){
+    var first = first
+    var second = second
+        if (first<second) {return 1}else{return 0};
+};
+
+function orderPlayDesen (first, second) {
+    var first = first
+    var second = second
+        if (first<second) {return 0}else{return 1};
+};
+
+function orderAlphabetic (a, b) {
+    var A = a
+    var B = b
+        if (A < B){return -1;}else if (A > B){return 1;}else{return 0;}
+};
+function orderAlphabeticZA (a, b) {
+    var A = a
+    var B = b
+        if (A > B){return -1;}else if (A < B){return 1;}else{return 0;}
 };
 
 jQuery(document).ready(function($) {
@@ -29,7 +52,9 @@ jQuery(document).ready(function($) {
         playCounts.push(parsed_json["tracks"]["track"][i]["playcount"])
         playorder.push(parsed_json["tracks"]["track"][i]["playcount"])
         varDuration.push(parsed_json["tracks"]["track"][i]["duration"])
+        durationOrder.push(parsed_json["tracks"]["track"][i]["duration"])
         varName.push(parsed_json["tracks"]["track"][i]["name"])
+        namOrder.push(parsed_json["tracks"]["track"][i]["name"])
         varArtist.push(parsed_json['tracks']['track'][i]['artist']['name'])
         try {varImage.push(parsed_json["tracks"]["track"][i]["image"][2]["#text"])}
         catch(err){varImage.push("images/undefined.jpg")}
@@ -60,42 +85,20 @@ jQuery(document).ready(function($) {
 });
 
  $("#counts").click(
-      function(event) {
-event.preventDefault();
+      function(event) {event.preventDefault();
 $(".songs").slideDown();
  $(".ord").slideUp();
+        $(".up").show("fast");
+        $(".down").hide("fast");
         $("#Top").removeClass("selecfiltr");
-        $("#counts").addClass("selecfiltr");
         $("#time").removeClass("selecfiltr");
         $("#alpha").removeClass("selecfiltr");
         $(".oculto").removeClass("selecfiltr");
-        $("#top").removeClass("selecfiltr");
-
-
-      function(event) {
-        $(".oculto").addClass("selecfiltr");
-        $("#alpha").addClass("selecfiltr");
-        $("#counts").removeClass("selecfiltr");
-        $("#time").removeClass("selecfiltr");
-        $("#Top").removeClass("selecfiltr");
-        $("#alpha").click(function(event){
-        event.preventDefault();
-        $(".oculto").show("fast");
-        $(".muestra").hide("fast");
-        });
-        $(".oculto").click(function(event){
-        event.preventDefault();
-        $(".muestra").show(100);
-        $(".oculto").hide(100);
-        $(".oculto").addClass("selecfiltr");
-        $("#alpha").addClass("selecfiltr");
-        $("#counts").removeClass("selecfiltr");
-        $("#time").removeClass("selecfiltr");
-        $("#Top").removeClass("selecfiltr");
-        });
-
-
-        playCounts.sort(orderplay);
+        $("#counts").addClass("selecfiltr");
+        $(".up").addClass("selecfiltr");
+        $("#counts2").addClass("selecfiltr");
+        $("#time2").removeClass("selecfiltr");
+        playCounts.sort(orderPlay);
         for (var s = 0; s <50; s++) {
             for (var i = 0; i <50; i++) {
         if (playCounts[s] === playorder[i]) {
@@ -122,135 +125,281 @@ $(".songs").slideDown();
         };
             };
         };
+    console.log("orden countplay decendente")
+});
+
+$("#counts2").click(function(event){event.preventDefault();
+$(".songs").slideDown();
+ $(".ord").slideUp();
+    $(".down").show(100);
+    $(".up").hide(100);
+    $("#Top").removeClass("selecfiltr");
+    $("#time").removeClass("selecfiltr");
+    $("#alpha").removeClass("selecfiltr");
+    $(".oculto").removeClass("selecfiltr");
+    $("#counts").removeClass("selecfiltr");
+    $("#down").addClass("selecfiltr");
+    $("#time2").removeClass("selecfiltr");
+        $("#counts2").addClass("selecfiltr");
+        playCounts.sort(orderPlayDesen);
+        for (var s = 0; s <50; s++) {
+            for (var i = 0; i <50; i++) {
+        if (playCounts[s] === playorder[i]) {
+            console.log(playCounts)
+     $('.songs').append(
+    "<div class='col-md-8 col-md-offset-1 ord'>"+
+        "<hr/>"+
+        "<div class='col-md-2'>"+
+            "<p class='noSong'>"+(i+1)+"</p>"+
+        "</div>"+
+        "<div class='col-md-2'>"+
+            "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+        "</div>"+
+        "<div class='col-md-8'>"+
+            "<div class='infoSong'>"+
+                "<p class='name'>"+"Name  : "+varName[i]+"</classp>"+
+                "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                "<p class='duration'>"+"Duration  : "+varDuration[i]+"  Seg."+"</p>"+
+                "<p class='playCounts'>"+"Play counts  : "+playorder[i]+"</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"
+      );
+        };
+            };
+        };
+    console.log("orden countplay acendente")
+    });
+
+ $("#time").click(function(event) {event.preventDefault();
+$(".songs").slideDown();
+ $(".ord").slideUp();
+$(".timer2").show("fast");
+$(".timer1").hide("fast");
+        $("#Top").removeClass("selecfiltr");
+        $("#time").addClass("selecfiltr");
+        $("#time2").addClass("selecfiltr");
+        $("#counts").removeClass("selecfiltr");
+        $("#alpha").removeClass("selecfiltr");
+        $(".oculto").removeClass("selecfiltr");
+        $("#top").removeClass("selecfiltr");
+        $("#counts2").removeClass("selecfiltr");
+        varDuration.sort(orderPlay);
+        for (var s = 0; s <50; s++) {
+            for (var i = 0; i <50; i++) {
+        if (varDuration[s] === durationOrder[i]) {
+            console.log(varDuration)
+     $('.songs').append(
+    "<div class='col-md-8 col-md-offset-1 ord'>"+
+        "<hr/>"+
+        "<div class='col-md-2'>"+
+            "<p class='noSong'>"+(i+1)+"</p>"+
+        "</div>"+
+        "<div class='col-md-2'>"+
+            "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+        "</div>"+
+        "<div class='col-md-8'>"+
+            "<div class='infoSong'>"+
+                "<p class='name'>"+"Name  : "+varName[i]+"</classp>"+
+                "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                "<p class='duration'>"+"Duration  : "+durationOrder[i]+"  Seg."+"</p>"+
+                "<p class='playCounts'>"+"Play counts  : "+playorder[i]+"</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"
+      );
+        };
+            };
+        console.log(varDuration)
+        console.log(durationOrder);
+        console.log("bien")
+        };
+    console.log("orden duration decendente")
+    });
+
+$("#time2").click(function(event){
+    event.preventDefault();
+$(".songs").slideDown();
+$(".ord").slideUp();
+$(".timer1").show(100);
+$(".timer2").hide(100);
+    $("#Top").removeClass("selecfiltr");
+    $("#time").addClass("selecfiltr");
+    $("#time2").addClass("selecfiltr");
+    $("#counts").removeClass("selecfiltr");
+    $("#alpha").removeClass("selecfiltr");
+    $(".oculto").removeClass("selecfiltr");
+    $("#top").removeClass("selecfiltr");
+    $("#counts2").removeClass("selecfiltr");
+        varDuration.sort(orderPlayDesen);
+        for (var s = 0; s <50; s++) {
+            for (var i = 0; i <50; i++) {
+        if (varDuration[s] === durationOrder[i]) {
+            console.log(varDuration)
+     $('.songs').append(
+    "<div class='col-md-8 col-md-offset-1 ord'>"+
+        "<hr/>"+
+        "<div class='col-md-2'>"+
+            "<p class='noSong'>"+(i+1)+"</p>"+
+        "</div>"+
+        "<div class='col-md-2'>"+
+            "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+        "</div>"+
+        "<div class='col-md-8'>"+
+            "<div class='infoSong'>"+
+                "<p class='name'>"+"Name  : "+varName[i]+"</classp>"+
+                "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                "<p class='duration'>"+"Duration  : "+durationOrder[i]+"  Seg."+"</p>"+
+                "<p class='playCounts'>"+"Play counts  : "+playorder[i]+"</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"
+      );
+        };
+            };
+            console.log("hola qu hace");
+        };
+    console.log("orden duration acendente")
 });
 
 
-
-/*
-public class Prueba {
-  public static void main(String[] args) {
-
-    String a[] = {"Ramon", "Maria", "Juan"};
-
-    ArrayList lista = new ArrayList(); 
-
-    for(int i = 0; i < a.length; i++){
-
-      lista.add(a[i]);
-
-    }
-
-    Collections.sort(lista);
-
-    System.out.println(lista);
-
-
-
-  }
-
-}*/
-
-
-
-
-
-
-
-
-/*
-
- $("#Top").click(
-      function(ListTop) {
-        $("#Top").addClass("selecfiltr");
-        $("#time").removeClass("selecfiltr");
-        $("#alpha").removeClass("selecfiltr");
-        $(".oculto").removeClass("selecfiltr");
-        $("#counts").removeClass("selecfiltr");
-
-    for (var i = 0; i <= 50; i++) {
-        playCounts.push(ListTop["tracks"]["track"][i]["playcount"])
-        varDuration.push(ListTop["tracks"]["track"][i]["duration"])
-        varName.push(ListTop["tracks"]["track"][i]["name"])
-        varArtist.push(ListTop['tracks']['track'][i]['artist']['name'])
-        try {varImage.push(ListTop["tracks"]["track"][i]["image"][2]["#text"])}
-        catch(err){varImage.push("images/undefined.jpg")}
-
-            console.log(playCounts[i]);
-            console.log(varImage[i]);
-
-        for(s=0; s<playCounts[i].length; s++){
-            console.log(playCounts[s]);
-            }
-
-          $('.songs').append(
-        "<div class='col-md-8 col-md-offset-1'>"+
-            "<hr/>"+
-            "<div class='col-md-2'>"+
-                "<p class='noSong'>"+(i+1)+"</p>"+
-            "</div>"+
-            "<div class='col-md-2'>"+
-                "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
-            "</div>"+
-            "<div class='col-md-8'>"+
-                "<div class='infoSong'>"+
-                    "<p class='name'>"+"Name  : "+varName[i]+"</classp>"+
-                    "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
-                    "<p class='duration'>"+"Duration  : "+varDuration[i]+"  Seg."+"</p>"+
-                    "<p class='playCounts'>"+"Play counts  : "+playCounts[i]+"</p>"+
-                "</div>"+
-            "</div>"+
-        "</div>"
-            );
-}
-    });
-
-
- $("#counts").click(
-      function() {
-        $("#Top").removeClass("selecfiltr");
-        $("#counts").addClass("selecfiltr");
-        $("#time").removeClass("selecfiltr");
-        $("#alpha").removeClass("selecfiltr");
-        $(".oculto").removeClass("selecfiltr");
-        $("#top").removeClass("selecfiltr");
-    });
-
-
-
- $("#time").click(
-      function() {
-        $("#Top").removeClass("selecfiltr");
-        $("#time").addClass("selecfiltr");
-        $("#counts").removeClass("selecfiltr");
-        $("#alpha").removeClass("selecfiltr");
-        $(".oculto").removeClass("selecfiltr");
-        $("#top").removeClass("selecfiltr");
-    });
 
 
 
  $("#alpha").click(
       function(event) {
-        $(".oculto").addClass("selecfiltr");
-        $("#alpha").addClass("selecfiltr");
-        $("#counts").removeClass("selecfiltr");
-        $("#time").removeClass("selecfiltr");
-        $("#Top").removeClass("selecfiltr");
-        $("#alpha").click(function(event){
-        event.preventDefault();
+$(".songs").slideDown();
+$(".ord").slideUp();
         $(".oculto").show("fast");
         $(".muestra").hide("fast");
-        });
-        $(".oculto").click(function(event){
-        event.preventDefault();
-        $(".muestra").show(100);
-        $(".oculto").hide(100);
         $(".oculto").addClass("selecfiltr");
         $("#alpha").addClass("selecfiltr");
         $("#counts").removeClass("selecfiltr");
         $("#time").removeClass("selecfiltr");
         $("#Top").removeClass("selecfiltr");
-        });
+        $("#counts2").removeClass("selecfiltr");
+        $("#time2").removeClass("selecfiltr");
+        varName.sort(orderAlphabetic);
+        for (var s = 0; s <50; s++) {
+            for (var i = 0; i <50; i++) {
+        if (varName[s] === namOrder[i]) {
+            console.log(varName)
+     $('.songs').append(
+    "<div class='col-md-8 col-md-offset-1 ord'>"+
+        "<hr/>"+
+        "<div class='col-md-2'>"+
+            "<p class='noSong'>"+(i+1)+"</p>"+
+        "</div>"+
+        "<div class='col-md-2'>"+
+            "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+        "</div>"+
+        "<div class='col-md-8'>"+
+            "<div class='infoSong'>"+
+                "<p class='name'>"+"Name  : "+namOrder[i]+"</classp>"+
+                "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                "<p class='duration'>"+"Duration  : "+durationOrder[i]+"  Seg."+"</p>"+
+                "<p class='playCounts'>"+"Play counts  : "+playorder[i]+"</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"
+      );
+        };
+            };
+            console.log("si ordena alfabeticamente a-z");
+        };
+    console.log("orden a-z")
+});
 
-    });*/
+$("#zeta").click(function(event){
+    event.preventDefault();
+$(".songs").slideDown();
+$(".ord").slideUp();
+    $(".muestra").show(100);
+    $(".oculto").hide(100);
+    $(".oculto").addClass("selecfiltr");
+    $("#alpha").addClass("selecfiltr");
+    $("#counts").removeClass("selecfiltr");
+    $("#time").removeClass("selecfiltr");
+    $("#Top").removeClass("selecfiltr");
+    $("#counts2").removeClass("selecfiltr");
+    $("#time2").removeClass("selecfiltr");
+        varName.sort(orderAlphabeticZA);
+        for (var s = 0; s <50; s++) {
+            for (var i = 0; i <50; i++) {
+        if (varName[s] === namOrder[i]) {
+            console.log(varName)
+     $('.songs').append(
+    "<div class='col-md-8 col-md-offset-1 ord'>"+
+        "<hr/>"+
+        "<div class='col-md-2'>"+
+            "<p class='noSong'>"+(i+1)+"</p>"+
+        "</div>"+
+        "<div class='col-md-2'>"+
+            "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+        "</div>"+
+        "<div class='col-md-8'>"+
+            "<div class='infoSong'>"+
+                "<p class='name'>"+"Name  : "+namOrder[i]+"</classp>"+
+                "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                "<p class='duration'>"+"Duration  : "+durationOrder[i]+"  Seg."+"</p>"+
+                "<p class='playCounts'>"+"Play counts  : "+playorder[i]+"</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"
+      );
+        };
+            };
+            console.log("si ordena alfabeticamente z-a");
+        };
+    console.log("orden z-a")
+});
+
+ $("#Top").click(
+      function(event) {
+    event.preventDefault();
+$(".songs").empty();
+
+        $("#Top").addClass("selecfiltr");
+        $("#time").removeClass("selecfiltr");
+        $("#alpha").removeClass("selecfiltr");
+        $(".oculto").removeClass("selecfiltr");
+        $("#counts").removeClass("selecfiltr");
+        $("#time2").removeClass("selecfiltr");
+        $("#counts2").removeClass("selecfiltr");
+    for (var i = 0; i <= 50; i++) {
+        playCounts.push(parsed_json["tracks"]["track"][i]["playcount"])
+        playorder.push(parsed_json["tracks"]["track"][i]["playcount"])
+        varDuration.push(parsed_json["tracks"]["track"][i]["duration"])
+        durationOrder.push(parsed_json["tracks"]["track"][i]["duration"])
+        varName.push(parsed_json["tracks"]["track"][i]["name"])
+        namOrder.push(parsed_json["tracks"]["track"][i]["name"])
+        varArtist.push(parsed_json['tracks']['track'][i]['artist']['name'])
+        try {varImage.push(parsed_json["tracks"]["track"][i]["image"][2]["#text"])}
+        catch(err){varImage.push("images/undefined.jpg")}
+            console.log(playCounts[i]);
+            console.log(varImage[i]);
+      $('.songs').append(
+    "<div class='col-md-8 col-md-offset-1 ord'>"+
+        "<hr/>"+
+        "<div class='col-md-2'>"+
+            "<p class='noSong'>"+(i+1)+"</p>"+
+        "</div>"+
+        "<div class='col-md-2'>"+
+            "<div class='images'>"+"<img src=\""+varImage[i]+"\" class='imgbig'>"+"</div>"+
+        "</div>"+
+        "<div class='col-md-8'>"+
+            "<div class='infoSong'>"+
+                "<p class='name'>"+"Name  : "+varName[i]+"</classp>"+
+                "<p class='artist'>"+"Artist name  : "+varArtist[i]+"</p>"+
+                "<p class='duration'>"+"Duration  : "+varDuration[i]+"  Seg."+"</p>"+
+                "<p class='playCounts'>"+"Play counts  : "+playorder[i]+"</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"
+      );
+    }
+    });
+
+
+
 
